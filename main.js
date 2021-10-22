a4af176aca9b124728419737edee2e5f3980ebfe
pedro_galhardi <61712871+pedrogalhardi@users.noreply.github.com>
2021-10-22 12:43:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const LinkSocialMedia = {
+  linkedin: 'in/pedro-galhardi-5b8402197',
+  github: 'pedrogalhardi',
+  instagram: 'galhardi.pedro',
+  facebook: 'pedrohenrique.galhardi'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinkSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinkSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userPhoto.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
